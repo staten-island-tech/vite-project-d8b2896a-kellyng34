00ae9512a0662f473af47fb9b89c83573d047acc
Kelly Ng <kellyng19lover@gmail.com>
2023-11-15 10:13:36 -0500

    sf
    sf

---
+++ poop/js/app.js
@@ -0,0 +1,65 @@
+const fruits= [
+{
+    name: banana,
+    price: $1,
+    pic: banana.jpeg
+},
+{
+    name: apple,
+    price: $2,
+    pic:apple.jpeg
+},
+{
+    name: orange,
+    price: $2,
+    pic: orange.jpeg
+},
+{
+    name: strawberries,
+    price: $1,
+    pic: strawberries.jpeg
+},
+{
+    name: rasberries,
+    price: $1,
+    pic: rasberries.jpeg
+},
+{
+    name: mango,
+    price: $1,
+    pic: mango.jpeg
+}
+]
+
+const vegetables=[
+{
+    name: carrots,
+    price: $2,
+    pic:na
+},
+{
+    name: potato,
+    price: $1,
+    pic:na
+},
+{
+    name: cucumber,
+    price: $1,
+    pic:na
+},
+{
+    name:sweetpotato,
+    price: $2,
+    pic:na
+},
+{
+    name: avocado,
+    price: $2,
+    pic:na
+},
+{
+    name: lettuce,
+    price: $1,
+    pic:na
+}
+]
