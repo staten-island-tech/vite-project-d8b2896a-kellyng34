f450548438b9e17a96a3eeaa727843ef5a09dbdd
Kelly Ng <kellyng19lover@gmail.com>
2023-11-22 09:54:31 -0500

    eg
    sg

--- poop/js/counter.js
+++ poop/js/counter.js
@@ -1,13 +1,4 @@
-`export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
-`
+
 
 const DOMSelectors = {
   form: document.querySelector("#form"),
